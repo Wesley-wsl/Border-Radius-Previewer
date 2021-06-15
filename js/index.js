da3fcd30d96c05da8378fed5329c14dd6afc1c05
Wesley Westelley <wesleywestelley@gmail.com>
2021-06-15 18:09:57 -0300

    created html,css,js and first modification at all

---
+++ js/index.js
@@ -0,0 +1,4 @@
+let $topLeft = document.querySelector('[data-position="top-left"]')
+let $topRight = document.querySelector('[data-position="top-right"]')
+let $bottomRight = document.querySelector('[data-position="bottom-right"]')
+let $bottomLeft = document.querySelector('[data-position="bottom-left"]')
