56bf63f648523cdc23e9335e329435e5139eddc9
Wesley Westelley <wesleywestelley@gmail.com>
2021-06-16 17:22:12 -0300

    finish

--- js/index.js
+++ js/index.js
@@ -2,3 +2,23 @@ let $topLeft = document.querySelector('[data-position="top-left"]')
 let $topRight = document.querySelector('[data-position="top-right"]')
 let $bottomRight = document.querySelector('[data-position="bottom-right"]')
 let $bottomLeft = document.querySelector('[data-position="bottom-left"]')
+let $previewer = document.querySelector('#Previewer')
+let $code = document.querySelector('.code')
+
+
+$topLeft.addEventListener('keyup', changeBorders)
+$topRight.addEventListener('keyup', changeBorders)
+$bottomRight.addEventListener('keyup', changeBorders)
+$bottomLeft.addEventListener('keyup', changeBorders)
+
+
+function changeBorders() {
+    $previewer.style.borderRadius = `${$topLeft.value}px ${$topRight.value}px ${$bottomRight.value}px ${$bottomLeft.value}px`
+    $code.value = `border-radius: ${$topLeft.value}px ${$topRight.value}px ${$bottomRight.value}px ${$bottomLeft.value}px`
+}
+
+function clipboard() {
+    $code.select()
+    document.execCommand("Copy")
+    alert("Copiado :)")
+}
